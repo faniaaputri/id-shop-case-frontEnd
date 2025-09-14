dbb29c830614e26726e747eab64836f17af14454
DzakwanRizky7 <dzakwanrizky04@gmail.com>
2025-09-14 18:21:02 +0700

    Masukin Gambar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,206 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center">
+      <Navbar />
+      <div className="relative h-[80vh] w-screen ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"/images/hero-section.jpeg"}
+          fill
+          alt="hero-section-image"
+          className="object-cover object-top"
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </div>
+      <section className="h-[85vh] w-screen flex flex-col justify-center">
+        <h1 className="font-black text-3xl mb-20 text-center">
+          IDSHOP Customization
+        </h1>
+        <div className="w-full px-10 grid grid-cols-4 justify-items-center">
+          {products.map((product) => (
+            <CardProduct key={product.id} {...product} />
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </section>
+      <SectionLayout
+        title="Hero Section"
+        image="/images/image-section-1.jpg"
+        alt="image-section-1"
+        isLeft={true}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio facere
+        est commodi cumque, illum sit eveniet, fugit quam doloribus debitis,
+        iure vel laboriosam corporis earum minima? Non facilis a modi expedita
+        impedit enim doloribus quaerat iure minima iste vitae laudantium
+        voluptates consequuntur aliquid, commodi deserunt quod odit atque nihil
+        ipsa veritatis! Tempora iusto amet repellat iste neque, quod
+        voluptatibus inventore nulla saepe pariatur animi hic harum sed
+        reiciendis ullam aperiam perferendis aut vel quibusdam numquam
+        repellendus nostrum architecto in. Illum corporis maiores, inventore in
+        quisquam illo autem qui veritatis aliquam cum voluptatum alias, ipsum
+        repudiandae quam voluptatibus repellat. Corporis, ea.
+      </SectionLayout>
+      <SectionLayout
+        title="Show Off Your Personality With a Custom Case"
+        image="/images/product-3.jpeg"
+        alt="image-section-2"
+        isLeft={false}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ratione
+        soluta, modi odit debitis ut a ipsa qui accusantium nulla vel reiciendis
+        eaque eum, harum at obcaecati ex quas. Quae non recusandae possimus ab,
+        similique cum corporis consequatur totam laudantium ratione aliquam
+        velit unde minima earum, laboriosam nemo ullam soluta eius illo nostrum!
+        Ut autem fugiat sit delectus suscipit nemo odit quis similique ratione,
+        sint provident, nostrum recusandae earum reprehenderit!
+      </SectionLayout>
+      <section className="h-screen w-screen py-6 px-16 flex flex-row gap-3">
+        <div className="w-2/6 h-full bg-white rounded-2xl overflow-hidden  border-2">
+          <div className="w-full h-1/2 flex flex-col justify-between px-3 pt-8 pb-3">
+            <div>
+              <h2 className="text-4xl font-heading font-black my-5">
+                Hero Section
+              </h2>
+              <p className="text-foreground font-garamond text-2xl w-[90%]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+                autem! Officia culpa praesentium delectus atque. Est minima
+                iusto vero quidem!
+              </p>
+            </div>
+            <div className="flex justify-end">
+              <Button
+                variant={"outline"}
+                size={"xl"}
+                className="bg-gradient-to-r from-muted-foreground to-foreground px-10 text-2xl font-medium"
+              >
+                Custom Now
+              </Button>
+            </div>
+          </div>
+          <div className="relative w-full h-1/2">
+            <Image
+              src={"/images/image-section-3-1.jpg"}
+              fill
+              alt="Image-Section-3"
+              className="object-cover object-center"
+            ></Image>
+          </div>
+        </div>
+        <div className="relative w-4/6 h-full bg-blue-400 rounded-2xl overflow-hidden">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+            src={"/images/product-1.jpeg"}
+            alt="Image-Section-3"
+            fill
+            className="object-cover"
+          ></Image>
+          <div className="top-0 right-0 absolute h-full w-1/2 bg-black/[76%] backdrop-blur-lg rounded-2xl flex flex-col justify-between px-5 pt-8 pb-5">
+            <div className="text-white">
+              <h2 className="text-4xl font-heading font-black my-5">
+                Hero Section
+              </h2>
+              <p className="font-garamond text-2xl w-[90%]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                est sunt adipisci amet praesentium nostrum tenetur voluptatem
+                inventore dolore corrupti velit similique ipsa, dicta illum
+                facere distinctio ut dolorem, quidem temporibus voluptatibus
+                accusantium laudantium veniam deserunt? Explicabo recusandae
+                laborum fugit quo error, ut cum soluta aspernatur provident,
+                optio mollitia ipsa.
+              </p>
+            </div>
+            <div className="flex justify-end">
+              <Button
+                size={"xl"}
+                variant={"outline"}
+                className="px-10 text-2xl font-medium"
+              >
+                Custom Now
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="h-screen w-screen py-6 px-16 flex flex-row gap-3">
+        <div className="w-[30%] h-full rounded-2xl overflow-hidden  ">
+          <div className="relative w-full h-full ">
+            <Image
+              src={"/images/product-4.jpeg"}
+              alt="Image-Section-4"
+              fill
+              className="object-cover"
+            ></Image>
+            <div className="absolute bottom-0 bg-black/75 backdrop-blur-lg left-0 w-full h-2/5 flex flex-col justify-between px-5 pt-2 pb-5">
+              <div className="text-white">
+                <h2 className="text-4xl font-heading font-black my-5">
+                  Make Your Own Keychain
+                </h2>
+                <p className="font-garamond text-2xl w-[90%]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  est sunt adipisci amet praesentium nostrum tenetur voluptatem
+                </p>
+              </div>
+              <div className="flex justify-end">
+                <Button
+                  size={"xl"}
+                  variant={"outline"}
+                  className="px-10 text-2xl font-medium"
+                >
+                  Custom Now
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[70%] h-full rounded-2xl overflow-hidden">
+          <div className="relative w-full h-full ">
+            <Image
+              src={"/images/image-section-4-1.jpeg"}
+              alt="Image-Section-4"
+              fill
+              className="object-cover"
+            ></Image>
+            <div className="absolute bottom-0 bg-black/75 backdrop-blur-lg left-0 w-full h-1/3 flex flex-col justify-between px-5 pt-2 pb-5">
+              <div className="text-white">
+                <h2 className="text-4xl font-heading font-black my-5">
+                  Create Your Creative Popsocket
+                </h2>
+                <p className="font-garamond text-2xl w-[90%]">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Amet, enim? Debitis earum sapiente enim reiciendis!
+                </p>
+              </div>
+              <div className="flex justify-end">
+                <Button
+                  size={"xl"}
+                  variant={"outline"}
+                  className="px-10 text-2xl font-medium bg-white text-foreground"
+                >
+                  Custom Now
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="w-screen h-[75vh] bg-foreground">
+        <div className="h-full flex items-center pb-36 px-20">
+          <div className="w-1/4 text-white flex flex-col gap-4">
+            <h2 className="font-bold text-4xl">IDshop</h2>
+            <ul>
+              <FooterLink href="/">Beranda</FooterLink>
+              <FooterLink href="/">Custom Case</FooterLink>
+              <FooterLink href="/">Tentang Kami</FooterLink>
+            </ul>
+          </div>
+          <div className="w-1/4 text-white flex flex-col gap-4">
+            <h2 className="font-bold text-4xl">Hubungi Kami</h2>
+            <ul>
+              <FooterLink href="/">Email: support@idshop.com</FooterLink>
+              <FooterLink href="/">WhatsApp: 0812-3456-7890</FooterLink>
+              <FooterLink href="/">Instagram: @idshop.official</FooterLink>
+            </ul>
+          </div>
+        </div>
       </footer>
     </div>
   );
